22fcf5f27361ed5711895d8d65a6c49eb7f6fcdc
SOUFIANEZAZA <72149789+SOUFIANEZAZA@users.noreply.github.com>
2020-10-02 19:51:14 +0100

    Update run.php
    Gg

--- run.php
+++ run.php
@@ -164,7 +164,7 @@ if($cookie){
 				echo "[~] ".count($today)." story reacted today\n";
 				echo "[~] Limit instagram api 2000 seen/day\n";
 				echo "[~] Sleep for 20 hours to bypass instagram limit\n";
-				sleep(50000);
+				sleep(72000);
 				echo "[~] End sleep...\n\n";
 			}
 		} while($loop == true);
